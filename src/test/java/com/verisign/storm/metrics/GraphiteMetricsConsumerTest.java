4286f4a5464aa05f595ae512623262a494bbbe52
Kevin Mao <kmao@verisign.com>
2015-05-15 00:07:45 -0400

    Corrected typo.

--- src/test/java/com/verisign/storm/metrics/GraphiteMetricsConsumerTest.java
+++ src/test/java/com/verisign/storm/metrics/GraphiteMetricsConsumerTest.java
@@ -10,7 +10,7 @@
 * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 * See the License for the specific language governing permissions and limitations under the License.
 *
-* See the NOTICE file distributed with this work for additional in-formation regarding copyright ownership.
+* See the NOTICE file distributed with this work for additional information regarding copyright ownership.
 */
 package com.verisign.storm.metrics;
 
